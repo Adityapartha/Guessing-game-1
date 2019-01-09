d22e1e809406fb9c6b10e76a709e8f8cec6589b7
aditya parthasarathy <surayasun59@gmail.com>
2019-01-08 17:01:15 -0800

    guessing game

---
+++ app.js
@@ -0,0 +1,63 @@
+// var user = prompt('what is your name');
+// console.log(user);
+
+// alert('Hi there I\m going to ask you few question');
+
+// var dogs = prompt('Do you have any dogs' + user + 'please answer with yes or no');
+//  var user=prompt('what is your name')
+// if (toUpperCase(dogs) === 'YES'){
+// }else {
+//     alert('dogs are the coolest animal');
+// }
+
+// var running = prompt('Is running your favorite?');
+// if (toLowercase(running) === 'yes')
+
+
+// var water = prompt ('Do you like drinking water'+ user );
+// if (toUpperCase(water) === 'NO'){
+// }else {
+//     alert('Its is healthly for you');
+// }
+// var coding = prompt('Do like coding?'+ user );
+// if(toUpperCase(coding) === 'yes')
+
+// var walking = prompt(user + 'do you walk every day to work?');
+// if (toLowercase(walking) === 'YES');
+var user = prompt('what is your name');
+console.log(user);
+
+alert("Hi there I'm going to ask you few question");
+
+var dogs = prompt('Do you have any dogs' + user + 'please answer with yes or no');
+
+
+var water = prompt ('Do you like drinking water'+ user );
+
+
+var running = prompt('Is running your favorite?');
+
+
+var coding = prompt('Do like coding?'+ user );
+
+
+var walking = prompt(user + 'do you walk every day to work?');
+
+if (toUpperCase(dogs) === 'YES'){
+}else {
+    alert('dogs are the coolest animal');
+}
+
+if (toUpperCase(water) === 'NO'){
+}else {
+    alert('Its is healthly for you');
+}
+
+if (toLowercase(running) === 'yes'){
+}else{
+
+}
+
+if(toUpperCase(coding) === 'yes')
+
+if (toLowercase(walking) === 'YES');
