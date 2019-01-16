5a75b81ae9ddbff170dd31e88daaf647cf88d248
aditya parthasarathy <surayasun59@gmail.com>
2019-01-16 12:11:28 -0800

    Aboutme

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
 
 "use strict"
-// var user = prompt('what is your name');
-// console.log(user);
-
-// alert('Hi there I\m going to ask you few question');
-
-// var dogs = prompt('Do you have any dogs' + user + 'please answer with yes or no');
-//  var user=prompt('what is your name')
-// if (toUpperCase(dogs) === 'YES'){
-// }else {
-//     alert('dogs are the coolest animal');
-// }
-
-// var running = prompt('Is running your favorite?');
-// if (toLowercase(running) === 'yes')
-
-
-// var water = prompt ('Do you like drinking water'+ user );
-// if (toUpperCase(water) === 'NO'){
-// }else {
-//     alert('Its is healthly for you');
-// }
-// var coding = prompt('Do like coding?'+ user );
-// if(toUpperCase(coding) === 'yes')
-
-// var walking = prompt(user + 'do you walk every day to work?');
-// if (toLowercase(walking) === 'YES');
 var score = 0
 var states = ['california' , 'florida', 'arizona', 'texas', 'nevada']
 var randowNumber = Math.floor(Math.random() * 21 );
